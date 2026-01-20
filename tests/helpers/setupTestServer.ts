541aa6825a0c30dbed7be3c92900e6bb1bf1b7fc
Gustavo Silva <gustavo.silva.nav@gmail.com>
2026-01-19 19:17:07 -0500

    📦️ Upgrading some packages to be up to date

--- tests/helpers/setupTestServer.ts
+++ tests/helpers/setupTestServer.ts
@@ -1,6 +1,6 @@
 import { logger } from '@adapters';
 import { customHeadersPlugin } from '@middlewares';
-import registerRoutes from '@server/routers';
+import { registerRoutes } from '@server/routers';
 import Fastify, { type FastifyBaseLogger } from 'fastify';
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
 
